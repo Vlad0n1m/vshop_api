872fb5caf3de7810b0753a0b943635ac0de9210a
l0xa1 <l0xa1ch@gmail.com>
2024-04-11 16:46:51 +0500

    qwe

--- productlist/views.py
+++ productlist/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Family, ProductList, Product, ProductItem
 from rest_framework import viewsets
-
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
 # Create your views here.
 from .serializers import  FamilySerializer, ProductListSerializer, ProductSerializer, ProductItemSerializer
 
@@ -21,3 +22,16 @@ class ProductViewSet(viewsets.ModelViewSet):
 class ProductItemViewSet(viewsets.ModelViewSet):
     queryset = ProductItem.objects.all()
     serializer_class = ProductItemSerializer
+    
+@require_GET
+def get_family_by_owner(request, owner_id):
+    try:
+        family = Family.objects.get(owner_id=owner_id)
+        # Assuming you have a serializer for your Family model
+        # Replace `FamilySerializer` with your actual serializer
+        serialized_family = FamilySerializer(family).data
+        return JsonResponse(serialized_family, status=200)
+    except Family.DoesNotExist:
+        return JsonResponse({'error': 'Family not found'}, status=404)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
