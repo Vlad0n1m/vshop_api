674f6d646d156e6dc94216de220d046950c84de4
l0xa1 <l0xa1ch@gmail.com>
2024-04-11 15:59:07 +0500

    models changed

--- productlist/models.py
+++ productlist/models.py
@@ -4,21 +4,12 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
     
-class Profile(models.Model):
-    user = models.OneToOneField(User, verbose_name="Пользователь", on_delete=models.CASCADE)
 
-@receiver(post_save, sender=User)
-def create_user_entity(sender, instance, created, **kwargs):
-    if created:
-        # Check if a profile already exists for this user
-        if not hasattr(instance, 'profile'):
-            profile = Profile(user=instance)
-            profile.save()
         
 # Create your models here.
 class Family(models.Model):
     uid = models.UUIDField(max_length=255, default=uuid4(), unique=True, verbose_name='Уникальный идентификатор')
-    owner = models.OneToOneField(Profile, verbose_name="Владелец семьи",  on_delete=models.CASCADE)
+    owner = models.OneToOneField(User, verbose_name="Владелец семьи",  on_delete=models.CASCADE)
     name = models.CharField(max_length=50, verbose_name="Название Семьи")
     
     def __str__(self) -> str:
@@ -34,9 +25,11 @@ class ProductList(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name="Наименование товара")
     price = models.IntegerField(verbose_name="Цена товара")
+    origin = models.CharField(max_length=255, verbose_name='Провайдер', default='default')
     
 class ProductItem(models.Model):
     product = models.ForeignKey(Product, verbose_name='Товар',  on_delete=models.CASCADE)
     count = models.IntegerField(verbose_name="Количество")
+    image = models.ImageField(upload_to='product_item_images', verbose_name='Картинка', null=True, blank=True)
     date_created = models.DateTimeField(auto_created=True, verbose_name="Дата создания")
-    who_added = models.ForeignKey(Profile, verbose_name="Пользователь добавивший", on_delete=models.CASCADE)
+    who_added = models.ForeignKey(User, verbose_name="Пользователь добавивший", on_delete=models.CASCADE)
